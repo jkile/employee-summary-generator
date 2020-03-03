b49626b28cf2d17f8e5ee8af41f9293c07c65232
Jake Kile <jkile9@gmail.com>
2020-03-02 19:26:31 -0700

    added starting classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+
+module.exports = class Employee{
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+    
+}
